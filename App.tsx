656a85759661cae96fe4fe3c49790cc98d61c84e
Mateus Henrique <mateushenriquechaves1@gmail.com>
2021-10-27 01:18:11 -0300

    redux and saga config

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+import '~/config/reactotron';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import Home from '~/screens/Home';
+
+import { persistor, store } from '~/store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Home />
+      </PersistGate>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
